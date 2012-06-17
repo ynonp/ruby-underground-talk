eda42a0843b85f55342560a03a588895b15460db
ynonp <ynonperek@gmail.com>
2012-06-17 17:47:56 +0300

    step 2

--- todo.rb
+++ todo.rb
@@ -2,14 +2,24 @@ require 'Qt'
 require './todo_ui'
 
 ##############################
+# MainWindow derived class is
+# our starting point to manage the app
+#
+
 class MainWindow < Qt::MainWindow
   def initialize( parent = nil )
+    # Note every displayable "thing" in Qt is
+    # called a QWidget, and every widget can have a parent
+    # If the parent is nil => widget is a window
+    # else widget is embedded in another widget
     super ( parent )
 
     @ui = Ui::MainWindow.new
     @ui.setup_ui ( self )
   end
 end
+
+##############################
 
 app = Qt::Application.new( ARGV )
 mw = MainWindow.new
